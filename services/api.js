8eeedb7967d423adbfd0bac6e8eca0e0cf9dfe4e
Whilack <leonid.gainar@gmail.com>
2022-01-11 11:21:13 +0200

    Changed axios baseURL to heroku

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://nuxt-express-todo-manager.netlify.app/api',
+  baseURL: 'https://nuxt-express-todo-manager.herokuapp.com/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
